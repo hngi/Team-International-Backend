d3c68c4875ed153c7ad9fe8c9bd8270ae96ddadd
athumani mwinami <mwinamijr@gmail.com>
2020-06-12 14:40:07 +0300

    changed swagger docs view

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework_swagger.views import get_swagger_view
 
-from .views import UserList
+from .views import UserList, schema_view
 
-schema_view = get_swagger_view(title="Docs")
+#schema_view = get_swagger_view(title="docs")
 
 urlpatterns = [
     path('api/team-international/user/logged', UserList.as_view(), name='users'),
--- users/views.py
+++ users/views.py
@@ -4,6 +4,16 @@ from rest_framework.response import Response
 import json
 
 
+from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import response, schemas
+
+@api_view()
+@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer])
+def schema_view(request):
+    generator = schemas.SchemaGenerator(title='Docs')
+    return response.Response(generator.get_schema(request=request))
+
 with open('user-data.json', 'r') as file:
 	f = file.read()
 
