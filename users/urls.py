43bef272496caf458cda69d300403bc7062d1d0d
athumani mwinami <mwinamijr@gmail.com>
2020-06-12 14:26:46 +0300

    added swagger docs url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import UserList
 
+schema_view = get_swagger_view(title="Docs")
+
 urlpatterns = [
     path('api/team-international/user/logged', UserList.as_view(), name='users'),
+    path('docs/', schema_view),
 ]
